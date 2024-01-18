e5f9c68ea5d7e12c4395b1bf41c26f11f8c773d5
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-01-18 10:29:35 -0600

    add guide

--- components/main.js
+++ components/main.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Story from './story'
 import { Image } from 'theme-ui'
-import { Row, Column } from '@carbonplan/components'
+import { Row, Column, Guide } from '@carbonplan/components'
 
 const Main = ({ mdxSource }) => {
   return (
     <>
+      <Guide />
       <Row>
-        <Column start={1} width={2}>
+        <Column start={1} width={3}>
           <Image src='/leap.png' alt='LEAP' sx={{ width: '100%' }} />
         </Column>
       </Row>
