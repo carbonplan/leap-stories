7548f99991bf6825c295cb059e1216b1af9a3838
katamartin <katasm@gmail.com>
2024-02-02 12:04:22 -0500

    Wire up basic pause/play

--- story/components/map.js
+++ story/components/map.js
@@ -1,14 +1,17 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef, useCallback } from 'react'
 import { Minimap, Path, Sphere, Raster } from '@carbonplan/minimaps'
 import { naturalEarth1 } from '@carbonplan/minimaps/projections'
-import { datasets } from '../datasets'
 import { useThemedColormap } from '@carbonplan/colormaps'
 import { Slider } from '@carbonplan/components'
 import zarr from 'zarr-js'
 import { Box } from 'theme-ui'
 
+import { datasets } from '../datasets'
+import PlayPause from './play-pause'
+
 const Map = ({ sourceUrl, variable, clim, colormapName }) => {
   const colormap = useThemedColormap(colormapName)
+  const [playing, setPlaying] = useState(false)
   const [time, setTime] = useState(0)
   const [data, setData] = useState([null])
   const [timeRange, setTimeRange] = useState([0, 0])
@@ -16,6 +19,7 @@ const Map = ({ sourceUrl, variable, clim, colormapName }) => {
   const [nullValue, setNullValue] = useState(9.969209968386869e36)
   const chunkCache = useRef({})
   const groupRef = useRef(null)
+  const timeout = useRef(null)
 
   useEffect(() => {
     const fetchGroup = async () => {
@@ -53,17 +57,56 @@ const Map = ({ sourceUrl, variable, clim, colormapName }) => {
     if (groupRef.current) {
       const chunkIndex = Math.floor(time / timeChunkSize)
       handleChunkLoading(chunkIndex)
-    }
-  }, [time, timeChunkSize])
 
-  const loadChunk = (getChunk, chunkIndex) => {
+      const nextChunkIndex = chunkIndex + 1
+      if (nextChunkIndex * timeChunkSize < timeRange[1]) {
+        handleChunkLoading(nextChunkIndex, false)
+      }
+    }
+  }, [time, timeChunkSize, timeRange])
+
+  const handlePlay = useCallback(
+    (willPlay) => {
+      if (timeout.current) {
+        clearTimeout(timeout.current)
+        timeout.current = null
+      }
+
+      setPlaying(willPlay)
+      if (willPlay) {
+        const incrementTime = () => {
+          timeout.current = setTimeout(() => {
+            let shouldContinue = true
+            setTime((prev) => {
+              let nextValue = prev + 1
+              if (nextValue > timeRange[1]) {
+                nextValue = timeRange[0]
+                shouldContinue = false
+                setPlaying(false)
+              }
+              return nextValue
+            })
+            if (shouldContinue) {
+              incrementTime()
+            }
+          }, 500)
+        }
+        incrementTime()
+      }
+    },
+    [timeRange]
+  )
+
+  const loadChunk = (getChunk, chunkIndex, shouldUpdateData) => {
     getChunk([chunkIndex, 0, 0], (err, chunk) => {
       if (err) {
         console.error('Error loading chunk:', err)
         return
       }
       chunkCache.current[chunkIndex] = chunk
-      updateData(chunkIndex)
+      if (shouldUpdateData) {
+        updateData(chunkIndex)
+      }
     })
   }
 
@@ -74,16 +117,16 @@ const Map = ({ sourceUrl, variable, clim, colormapName }) => {
     setData(chunkData)
   }
 
-  const handleChunkLoading = (chunkIndex) => {
-    if (chunkCache.current[chunkIndex]) {
+  const handleChunkLoading = (chunkIndex, shouldUpdateData = true) => {
+    if (chunkCache.current[chunkIndex] && shouldUpdateData) {
       updateData(chunkIndex)
     } else if (groupRef.current) {
-      loadChunk(groupRef.current[variable], chunkIndex)
+      loadChunk(groupRef.current[variable], chunkIndex, shouldUpdateData)
     }
   }
 
   const handleSliderChange = (event) => {
-    setTime(event.target.value)
+    setTime(parseInt(event.target.value))
   }
 
   return (
@@ -107,6 +150,7 @@ const Map = ({ sourceUrl, variable, clim, colormapName }) => {
         )}
       </Minimap>
       <Box sx={{ mt: 3 }}>
+        <PlayPause playing={playing} setPlaying={handlePlay} />
         <Slider
           min={timeRange[0]}
           max={timeRange[1]}
