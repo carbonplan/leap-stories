fdada4c672d5ec62b12eeb9a91c5630cbcc15044
katamartin <katasm@gmail.com>
2024-02-06 18:33:07 -0500

    Set up basic animation

--- story/components/sinks-exploration.js
+++ story/components/sinks-exploration.js
@@ -9,14 +9,16 @@ import {
 } from '@carbonplan/charts'
 import { useState } from 'react'
 import { Box } from 'theme-ui'
+import { animated, useSpring, easings } from '@react-spring/web'
 
 import { budgets } from '../data/carbon_budget_data'
+import { Filter } from '@carbonplan/components'
 
 const HEIGHT = 150
 const MAX_MAGNITUDE = 500
 const Y_SCALE = 10
 
-const BudgetCircle = ({ x, y, magnitude, negative, ...props }) => {
+const BudgetCircle = animated(({ x, y, magnitude, negative, ...props }) => {
   const { x: _x, y: _y } = useChart()
   const yFactor = negative ? -1 : 1
   const ratio = magnitude / MAX_MAGNITUDE
@@ -29,9 +31,9 @@ const BudgetCircle = ({ x, y, magnitude, negative, ...props }) => {
       {...props}
     />
   )
-}
+})
 
-const BudgetLabel = ({ x, magnitude, negative, ...props }) => {
+const BudgetLabel = animated(({ x, magnitude, negative, ...props }) => {
   const yFactor = negative ? -1 : 1
 
   return (
@@ -40,81 +42,86 @@ const BudgetLabel = ({ x, magnitude, negative, ...props }) => {
       y={((yFactor * magnitude) / MAX_MAGNITUDE) * Y_SCALE}
       align='center'
       verticalAlign={negative ? 'top' : 'bottom'}
-      width={2.5}
+      width={1.2}
       {...props}
     />
   )
-}
+})
 
 const SinksExploration = ({ debug = false }) => {
-  const [year, setYear] = useState(1851)
+  const [year, setYear] = useState(2022)
+  const { fossil, landUseChange, landSink, oceanSink } = useSpring({
+    fossil: budgets[0].values[year],
+    landUseChange: budgets[1].values[year],
+    landSink: budgets[2].values[year],
+    oceanSink: budgets[3].values[year],
+    config: {
+      duration: 500,
+      easing: easings.easeOut,
+    },
+  })
 
   return (
-    <Box sx={{ height: HEIGHT * 2 }}>
-      <Chart
-        x={[0, 10]}
-        y={[-Y_SCALE, Y_SCALE]}
-        padding={{ left: 0, bottom: 0 }}
-      >
-        <Grid horizontal values={[0]} sx={{ borderColor: 'primary' }} />
-        {debug && (
-          <>
-            <Grid horizontal vertical />
-            <TickLabels left />
-          </>
-        )}
-        <Plot>
-          <BudgetCircle x={2} y={0} magnitude={budgets[0].values[year]} />
-          <BudgetCircle
-            x={4}
-            y={0}
-            color='green'
-            magnitude={budgets[1].values[year]}
-          />
-          <BudgetCircle
-            x={6}
-            y={0}
-            color='green'
-            magnitude={budgets[2].values[year]}
-            negative
-          />
-          <BudgetCircle
-            x={8}
-            y={0}
-            color='blue'
-            magnitude={budgets[3].values[year]}
-            negative
-          />
-        </Plot>
-        <Label x={0} y={1.5}>
-          Sources
-        </Label>
-        <Label x={0} y={-0.5}>
-          Sinks
-        </Label>
-        <BudgetLabel x={2} magnitude={budgets[0].values[year]} color='primary'>
-          Fossil fuels
-        </BudgetLabel>
-        <BudgetLabel x={4} magnitude={budgets[1].values[year]} color='green'>
-          Land-use change
-        </BudgetLabel>
-        <BudgetLabel
-          x={6}
-          magnitude={budgets[2].values[year]}
-          negative
-          color='green'
+    <Box>
+      <Filter
+        values={{ start: year === 1851, end: year === 2022 }}
+        setValues={(obj) => setYear(obj.start ? 1851 : 2022)}
+        sx={{ mb: 3 }}
+      />
+      <Box sx={{ height: HEIGHT * 2 }}>
+        <Chart
+          x={[0, 10]}
+          y={[-Y_SCALE, Y_SCALE]}
+          padding={{ left: 0, bottom: 0 }}
         >
-          Land sink
-        </BudgetLabel>
-        <BudgetLabel
-          x={8}
-          magnitude={budgets[3].values[year]}
-          negative
-          color='blue'
-        >
-          Ocean sink
-        </BudgetLabel>
-      </Chart>
+          <Grid horizontal values={[0]} sx={{ borderColor: 'primary' }} />
+          {debug && (
+            <>
+              <Grid horizontal vertical />
+              <TickLabels left />
+            </>
+          )}
+          <Plot>
+            <BudgetCircle x={2} y={0} magnitude={fossil} />
+            <BudgetCircle x={4} y={0} color='green' magnitude={landUseChange} />
+            <BudgetCircle
+              x={6}
+              y={0}
+              color='green'
+              magnitude={landSink}
+              negative
+            />
+            <BudgetCircle
+              x={8}
+              y={0}
+              color='blue'
+              magnitude={oceanSink}
+              negative
+            />
+          </Plot>
+          <Label x={0} y={1.5}>
+            Sources
+          </Label>
+          <Label x={0} y={-0.5}>
+            Sinks
+          </Label>
+          <Label x={10} y={0} verticalAlign='middle' height={2} sx={{ pl: 2 }}>
+            {year}
+          </Label>
+          <BudgetLabel x={2} magnitude={fossil} color='primary'>
+            Fossil fuels
+          </BudgetLabel>
+          <BudgetLabel x={4} magnitude={landUseChange} color='green'>
+            Land-use change
+          </BudgetLabel>
+          <BudgetLabel x={6} magnitude={landSink} negative color='green'>
+            Land sink
+          </BudgetLabel>
+          <BudgetLabel x={8} magnitude={oceanSink} negative color='blue'>
+            Ocean sink
+          </BudgetLabel>
+        </Chart>
+      </Box>
     </Box>
   )
 }
