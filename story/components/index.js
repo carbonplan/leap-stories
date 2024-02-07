b9a7396a9cc013ccdafe7b7c4a54ac98bde1bbbe
katamartin <katasm@gmail.com>
2024-02-07 11:59:19 -0500

    Clean up bathtub

--- story/components/index.js
+++ story/components/index.js
@@ -1,4 +1,3 @@
-export { default as Bathtub } from './bathtub'
 export { default as Map } from './map'
 export { default as OceanCycleDiagram } from './ocean-cycle-diagram'
 export { default as Sinks } from './sinks'
