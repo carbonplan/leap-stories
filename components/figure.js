9b42dbd952a846d8cfac081a4cd84686d1553d4a
katamartin <katasm@gmail.com>
2024-03-21 17:08:56 -0400

    Render custom figure titles when provided

---
+++ components/figure.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Box, Flex } from 'theme-ui'
+
+const Figure = ({ as = 'figure', title, children, sx }) => {
+  return (
+    <Box
+      as={as}
+      sx={{
+        my: [6, 6, 6, 7],
+        '@media print': {
+          breakInside: 'avoid',
+        },
+        ...sx,
+      }}
+    >
+      {title && (
+        <Box
+          sx={{
+            fontSize: [3, 3, 3, 4],
+            fontFamily: 'heading',
+            fontWeight: 'heading',
+            lineHeight: 'h3',
+            mb: [3, 4],
+            pb: 2,
+            width: '100%',
+            borderWidth: 0,
+            borderColor: 'primary',
+            borderStyle: 'solid',
+            borderBottomWidth: 1,
+          }}
+        >
+          {title}
+        </Box>
+      )}
+      <Flex sx={{ flexDirection: 'column', gap: [4, 4, 4, 5] }}>
+        {children}
+      </Flex>
+    </Box>
+  )
+}
+
+export default Figure
--- components/index.js
+++ components/index.js
@@ -1,1 +1,2 @@
+export { default as Figure } from './figure'
 export { default as FigureCaption } from './figure-caption'
