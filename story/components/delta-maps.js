40f2e5ffd19d4afe0bfecc54e8cbabcdf315c2ac
katamartin <katasm@gmail.com>
2024-03-29 13:27:22 -0400

    Add custom projection logic

--- story/components/delta-maps.js
+++ story/components/delta-maps.js
@@ -23,6 +23,42 @@ const formatMonth = (month) => {
   })
 }
 
+const getCustomProjection = () => {
+  return {
+    ...naturalEarth1(),
+    glsl: {
+      func: `
+      vec2 naturalEarth1Invert(float x, float y)
+      {
+        const float pi = 3.14159265358979323846264;
+        const float halfPi = pi * 0.5;
+        float phi = y;
+        float delta;
+        float phi2 = phi * phi;
+        float phi4 = phi2 * phi2;
+        for (int i = 0; i < 25; i++) {
+          phi2 = phi * phi;
+          phi4 = phi2 * phi2;
+          delta = (phi * (1.007226 + phi2 * (0.015085 + phi4 * (-0.044475 + 0.028874 * phi2 - 0.005916 * phi4))) - y) / (1.007226 + phi2 * (0.015085 * 3.0 + phi4 * (-0.044475 * 7.0 + 0.028874 * 9.0 * phi2 - 0.005916 * 11.0 * phi4)));
+          phi = phi - delta;
+          if (abs(delta) < 1e-6) {
+            break;
+          }
+        }
+        phi2 = phi * phi;
+        float lambda = x / (0.8707 + phi2 * (-0.131979 + phi2 * (-0.013791 + phi2 * phi2 * phi2 * (0.003971 - 0.001529 * phi2))));
+        if (lambda <= -1.0 * pi + 1e-6 || lambda >= pi - 1e-6) {
+          return vec2(-1000.0, -1000.0);
+        } else {
+          return vec2(degrees(lambda), degrees(phi));
+        }
+      }
+    `,
+      name: 'naturalEarth1Invert',
+    },
+  }
+}
+
 const DeltaMaps = () => {
   const { theme } = useThemeUI()
   const colormap = useThemedColormap('warm')
@@ -86,7 +122,11 @@ const DeltaMaps = () => {
             1990
           </Box>
           <Box sx={{ mx: [-3, -3, -3, -5] }}>
-            <Minimap projection={naturalEarth1} scale={1} translate={[0, 0]}>
+            <Minimap
+              projection={getCustomProjection}
+              scale={1}
+              translate={[0, 0]}
+            >
               <Path
                 stroke={theme.colors.primary}
                 source={
@@ -125,7 +165,11 @@ const DeltaMaps = () => {
           </Box>
 
           <Box sx={{ mx: [-3, -3, -3, -5] }}>
-            <Minimap projection={naturalEarth1} scale={1} translate={[0, 0]}>
+            <Minimap
+              projection={getCustomProjection}
+              scale={1}
+              translate={[0, 0]}
+            >
               <Path
                 stroke={theme.colors.primary}
                 source={
