b8c89835f8334bc4c004a22c5e4de5850359db47
katamartin <katasm@gmail.com>
2024-04-01 14:47:08 -0400

    Add CarbonPlan credit

--- components/layout/authors.js
+++ components/layout/authors.js
@@ -1,12 +1,26 @@
+import { Button, Link } from '@carbonplan/components'
 import { Box } from 'theme-ui'
 
 const Authors = ({ authors }) => {
   return (
-    <Box sx={{ fontStyle: 'italic', mt: 5, mb: 6, fontSize: [3, 3, 3, 4] }}>
+    <Box sx={{ mt: 5, mb: 6, fontSize: [3, 3, 3, 4] }}>
       by{' '}
       {authors
         .map((a, i) => (i < authors.length - 1 ? a : `and ${a}`))
         .join(', ')}
+      <Box sx={{ fontStyle: 'italic', mt: [5, 5, 5, 6], color: 'secondary' }}>
+        This story was produced in collaboration with{' '}
+        <Button
+          href='https://carbonplan.org/'
+          inverted
+          sx={{ display: 'inline-block' }}
+        >
+          <Box as='span' sx={{ textDecoration: 'underline' }}>
+            CarbonPlan
+          </Box>
+          .
+        </Button>
+      </Box>
     </Box>
   )
 }
