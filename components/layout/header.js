993a4a05a0cb0706a155475cea260ca572d2c4d3
katamartin <katasm@gmail.com>
2024-05-06 14:40:04 -0400

    Render fixed header

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,29 +1,30 @@
 import React from 'react'
 import { Box, Link, Image } from 'theme-ui'
-import { Row, Column } from '@carbonplan/components'
 
 const Header = () => {
   return (
-    <Row
-      sx={{
-        pt: ['12px'],
-        pb: [3],
-      }}
-    >
-      <Column start={[1]} width={[2]}>
-        <Box
-          sx={{ pointerEvents: 'all', display: 'block', width: 'fit-content' }}
-        >
-          <Link
-            href='https://leap.columbia.edu/'
-            aria-label='LEAP Homepage'
-            sx={{ display: 'block' }}
-          >
-            <Image src='/leap.png' alt='LEAP' sx={{ width: '150px' }} />
-          </Link>
-        </Box>
-      </Column>
-    </Row>
+    <Box pb={8}>
+      <Box
+        as='header'
+        sx={{
+          width: '100%',
+          px: [4, 5, 5, 6],
+          borderBottom: '1px solid #eee',
+          borderColor: 'muted',
+          py: 3,
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 100,
+          backgroundColor: 'background',
+        }}
+      >
+        <Link href='/'>
+          <Image src={'/LEAP.png'} width={120} height={30} alt={'LEAP Logo'} />
+        </Link>
+      </Box>
+    </Box>
   )
 }
 
