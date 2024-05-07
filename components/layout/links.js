64c6d21b383ecd6085c3936acb929a1153d825d2
katamartin <katasm@gmail.com>
2024-05-07 12:17:34 -0400

    Make entire box clickable

--- components/layout/links.js
+++ components/layout/links.js
@@ -1,8 +1,5 @@
 import { Button, Column, Row } from '@carbonplan/components'
 import { RotatingArrow } from '@carbonplan/icons'
-import { Box } from 'theme-ui'
-
-// const COLORS = ['rgba(182,216,238,0.7)', 'rgba(0,119,102,0.6)'] // LEAP blue and teal
 
 const Links = ({ links }) => {
   const widths = links.length > 2 ? [3, 2, 2, 2] : [3, 3, 3, 3]
@@ -26,37 +23,21 @@ const Links = ({ links }) => {
           start={widths.map((w) => ((i * w) % 6) + 1)}
           width={widths}
         >
-          <Box
+          <Button
+            href={href}
+            suffix={<RotatingArrow />}
             sx={{
-              display: 'flex',
+              background: 'rgba(182,216,238,0.7)',
+              p: 5,
               width: '100%',
               height: '100%',
-              minHeight: [100],
-              alignItems: 'center',
-              // background: widths.map((w) => {
-              //   const entriesPerRow = 6 / w
-              //   const isEvenRowColumnSum =
-              //     (Math.floor(i / entriesPerRow) + (i % entriesPerRow)) % 2 ===
-              //     0
-              //   return isEvenRowColumnSum ? COLORS[0] : COLORS[1]
-              // }),
-              background: 'rgba(182,216,238,0.7)',
-              p: 4,
+              '&:hover': {
+                background: 'rgba(182,216,238,0.55)',
+              },
             }}
           >
-            <Button
-              href={href}
-              suffix={<RotatingArrow />}
-              sx={{
-                '&:hover': { color: 'background' },
-                '&:hover > #suffix-span >#suffix': {
-                  color: 'background',
-                },
-              }}
-            >
-              {label}
-            </Button>
-          </Box>
+            {label}
+          </Button>
         </Column>
       ))}
     </Row>
